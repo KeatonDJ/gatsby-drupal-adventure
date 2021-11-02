7070b55db0b4cb122f70324506cdc615f7f9d963
Keaton Johnson <keatondavidjohnson@gmail.com>
2021-11-01 21:07:27 -0500

    Added blt command hooks for node support.

---
+++ backend-drupal/blt/blt_scripts/frontend-assets.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Here goes nothing ¯\_(ツ)_/¯"
+RED='\033[0;31m' # Red
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+if [ -z "$1" ]; then echo "Please provide an npm package path."; exit 1; fi
+
+if command -v npm
+then
+    echo "Running build npm"
+    echo "Version: ${GREEN}" $(node -v)
+    echo $(npm run build --prefix $1)
+else
+    echo "${RED}NPM NOT FOUND. Front end will not function properly without node and NPM being installed!"
+fi
